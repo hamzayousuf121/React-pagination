7d0fa381ecf16dda1d143ce5c71de01a7d797e51
Hamza Yousuf <hamza.yousuf@treasurycube.com>
2020-11-21 14:33:40 +0500

    Updates

---
+++ precache-manifest.a1eae9164f0d95e810dfa1187878d7f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d58a10db38577c02c9ff1952a9bfb9",
+    "url": "/React-pagination/index.html"
+  },
+  {
+    "revision": "ca3702aa7d9cbbfe9a54",
+    "url": "/React-pagination/static/css/main.e88b7dc2.chunk.css"
+  },
+  {
+    "revision": "4dea63ec98f4a4958f92",
+    "url": "/React-pagination/static/js/2.02cc656a.chunk.js"
+  },
+  {
+    "revision": "ca3702aa7d9cbbfe9a54",
+    "url": "/React-pagination/static/js/main.662b0769.chunk.js"
+  },
+  {
+    "revision": "f8a898ec935f77506b13",
+    "url": "/React-pagination/static/js/runtime~main.bf5e41b2.js"
+  }
+]);
